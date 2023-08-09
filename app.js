8ff59b400c336350bfba1a7a855f2f449c2b7fea
Emanuelph23 <emanuelph6@gmail.com>
2023-08-09 20:59:24 -0300

    Feat: Config setup project

---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+const fileStore = require('session-file-store')(session);
+const flash = require('express-flash');
+
+const app = express();
+
+const conn = require('./db/conn');
+
+// Handlebars Middleware
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+
+//Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Express Session Middleware
+app.use(
+    session({
+        name: 'session',
+        secret: 'us_secret',
+        resave: false,
+        saveUninitialized: false,
+        store: new fileStore({
+            logFn: function () {},
+            path: require('path').join(require('os').tmpdir(), 'sessions'),
+        }),
+        cookie: {
+            secure: false,
+            maxAge: 360000,
+            expires: new Date(Date.now() + 360000),
+            httpOnly: true,
+        },
+    })
+);
+
+// Express Flash Messages Middleware
+app.use(flash());
+
+// Static Folder Middleware
+app.use(express.static('public'));
+
+//Set Session to Response
+app.use((req, res, next) => {
+
+    if(req.session.userid){
+        res.locals.session = req.session;
+    }
+
+    next();
+
+});
+
+conn
+    .sync()
+    .then(() => {
+        app.listen(3000, () => {
+            console.log('Server is running on port 3000');
+        });
+    })
+    .catch((error) => console.log(error));
